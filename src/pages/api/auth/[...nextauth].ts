7db6b33adab194da64c167eed5fac2126e5018da
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2022-02-14 01:53:34 -0300

    chore: implementar o FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
+import { query as q} from 'faunadb';
+
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -12,8 +15,38 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn(user, account, profile){
-      console.log(user);
-      return true;
+      const { email } = user
+
+      try {
+        
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true;
+      } catch {
+        return false;
+      }
     }
   }
 })
